a56d373d9b1b81f500a8a4b3749e038a841767da
God'sFavour Chukwudi <127259967+GChukwudi@users.noreply.github.com>
2024-03-10 12:04:01 +0200

    updated

--- backend/db/db.js
+++ backend/db/db.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 const db = async () => {
     try {
-        mongoose.set('strictQuery', false);
+        mongoose.set('strictQuery', false)
         await mongoose.connect(process.env.MONGO_URI)
         console.log('Connected to the db');
     } catch (error) {
-        console.log('Error connecting to the database');
+        console.log('Error connecting to the database' + error);
     }
 }
 
