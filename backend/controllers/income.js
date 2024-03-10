6bdc4008e6eed61d6a341f7418ea96844fad482b
God'sFavour Chukwudi <127259967+GChukwudi@users.noreply.github.com>
2024-03-10 22:32:41 +0200

    updated

--- backend/controllers/income.js
+++ backend/controllers/income.js
@@ -3,27 +3,27 @@ const IncomeSchema = require('../model/incomeModel')
 // Add income
 exports.addIncome = async (req, res) => {
     // console.log(req.body)
-    const {title, amount, category, description, date} = req.body
-
-    const income = IncomeSchema({
-        title,
-        amount,
-        category,
-        description,
-        date
-    })
+    const { title, amount, category, description, date } = req.body
     
     try {
         // validations
-        if(!title || !category || !description || !date) {
+        if(!title || !category || !description || !date || isNaN(amount) || amount <= 0) {
             return res.status(400).json({message: 'All fields are required'})
         }
-        if(amount <= 0 || !amount === Number) {
-            return res.status(400).json({message: 'Amount is required'})
-        }
+        const income = IncomeSchema({
+            title,
+            amount,
+            category,
+            description,
+            date
+        });
+
         await income.save()
         res.status(200).json({message: 'Income added successfully'})
-    }catch (error) {}
+    }catch (error) {
+        console.log('Error adding income', error)
+        res.status(500).json({message: 'Internal server error'})
+    }
 
     console.log(income)
 }
