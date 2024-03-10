429af0243c6f80388c096d2d74b1e532c0005802
God'sFavour Chukwudi <127259967+GChukwudi@users.noreply.github.com>
2024-03-10 09:54:52 +0200

    updated

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,10 @@
 
 require('dotenv').config()
 
-const PORT = process.env
+const PORT = process.env.PORT || 6000;
 
 const server = () => {
-    console.log(`Server is running on port: ${PORT}`)
+    console.log('Server is running on port:' PORT)
 }
 
 
