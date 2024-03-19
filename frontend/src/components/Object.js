866fb380e9a5a0c7d277350b64dae9a94d214aef
God'sFavour Chukwudi <127259967+GChukwudi@users.noreply.github.com>
2024-03-19 22:03:14 +0200

    updated

--- frontend/src/components/Object.js
+++ frontend/src/components/Object.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 
 function Object() {
+
+    const moveObject = keyframes`
+        0% {
+            transform: translate(0, 0%);
+        }
+        50% {
+            transform: translate(400px, 500px);
+        }
+        100% {
+            transform: translate(0, 0);
+        }
+    `;
 
     const ObjectStyled = styled.div`
     height: 70vh;
@@ -15,6 +27,7 @@ function Object() {
                                #F56692 0%,
                                #F2994A 100%);
     filter: blur(400px);
+    animation: ${moveObject} 1s alternate linear infinite;
 `; 
   return (
     <ObjectStyled></ObjectStyled>
