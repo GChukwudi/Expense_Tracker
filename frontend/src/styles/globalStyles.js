7fa8d2b91c2117ef70bdde6339007f91da1fbe36
God'sFavour Chukwudi <127259967+GChukwudi@users.noreply.github.com>
2024-03-19 20:38:38 +0200

    react app started

---
+++ frontend/src/styles/globalStyles.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        list-style: none;
+    }
+
+    :root {
+        --primary-color: #007bff;
+        --primary-color2: 'color: rgba(0, 123, 255, 0.5)';
+        --primary-color3: 'color: rgba(0, 123, 255, 0.1)';
+        --color-green: #28a745;
+        --color-grey: #6c757d;
+        --color-accent: #17a2b8;
+        --color-delete: #dc3545;
+    }
+`;
