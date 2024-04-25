fee49c0fd6b685f2f4e42b6879ebe3afe81df3fe
God'sFavour Chukwudi <127259967+GChukwudi@users.noreply.github.com>
2024-04-25 19:15:05 +0200

    updated

--- frontend/src/components/Incomes/Incomes.js
+++ frontend/src/components/Incomes/Incomes.js
@@ -1,12 +1,16 @@
 import React from "react";
 import styled from "styled-components";
-
+import { InnerLayout } from "../../styles/layouts";
 
 function Income() {
     return (
         <IncomesStyled>
             <innerHeight>
-
+                <h1>Incomes</h1>
+                <div className="income-content">
+                    <div className="form-container"></div>
+                    <div className="incomes"></div>
+                </div>
             </innerHeight>
         </IncomesStyled>
     )
---
+++ frontend/src/context/globalContext.js
@@ -0,0 +1,11 @@
+import React, {createContext} from "react";
+
+const GlobalContext = createContext();
+
+export const GlobalProvider = ({children} => {
+    return (
+        <GlobalContext.Provider>
+            {children}
+        </GlobalContext.Provider>
+    )
+});
