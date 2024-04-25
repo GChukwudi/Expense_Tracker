cb5d2f259d1010f554b18685ec31005a0fbc4c68
God'sFavour Chukwudi <127259967+GChukwudi@users.noreply.github.com>
2024-04-25 19:22:04 +0200

    updated

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -1,11 +1,14 @@
 import React, {createContext} from "react";
+import axios from "axios";
+
+const BASE_URL = "http://localhost:6000/api/v1/";
 
 const GlobalContext = createContext();
 
-export const GlobalProvider = ({children} => {
+export const GlobalProvider = ({children}) => {
     return (
         <GlobalContext.Provider>
             {children}
         </GlobalContext.Provider>
     )
-});
+}
