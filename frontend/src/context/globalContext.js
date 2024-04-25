2e28a22be071f3caa384ac1830d1945f3c258893
God'sFavour Chukwudi <127259967+GChukwudi@users.noreply.github.com>
2024-04-25 19:32:53 +0200

    updated

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -6,8 +6,21 @@ const BASE_URL = "http://localhost:6000/api/v1/";
 const GlobalContext = createContext();
 
 export const GlobalProvider = ({children}) => {
+
+    const [incomes, setIncomes] = useState([]);
+    const [expenses, setExpenses] = useState([]);
+    const [error, setError] = useState(null);
+
+    const addIncome = async (income) => {
+        const response = await axios.post(`${BASE_URL}add-income`, income)
+            .catch((err) => {
+                setError(err.response.data.message);
+            });
+    }
     return (
-        <GlobalContext.Provider>
+        <GlobalContext.Provider value={{
+            addIncome
+        }}>
             {children}
         </GlobalContext.Provider>
     )
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { GlobalStyles } from './styles/globalStyles';
+import { GlobalProvider } from './context/globalContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GlobalStyles />
-    <App />
+    <GlobalProvider>
+      <App />
+    </GlobalProvider>
   </React.StrictMode>
 );
 
